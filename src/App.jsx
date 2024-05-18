60e94aebd7a012a4d82ba2b3c6826781ada06934
kalu david <kaludavidinyang@gmail.com>
2024-05-18 20:55:31 +0100

    user Profile commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { UserProfile } from "./user-components/userProfile";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Mr. Dhave's Profile Card</h1>
+      <userProfile />
+    </div>
+  );
+}
---
+++ src/user-components/userProfile.jsx
@@ -0,0 +1,19 @@
+export function UserProfile() {
+  return (
+    <main id="User-Profile">
+      <p>UserName: Dhave</p>
+      <p>Email: kaludavidinyang@gmail.com</p>
+      <p>Phone: 08100002222</p>
+
+      <div>
+        <ul>
+          <li>Rice</li>
+
+          <li>Beans</li>
+
+          <li>Stew</li>
+        </ul>
+      </div>
+    </main>
+  );
+}
